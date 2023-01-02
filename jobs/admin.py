7543cfdca3a08415eb0ced405970b5a013e34951
reskyramadhandi <reskyramadhandi@gmail.com>
2023-01-02 11:30:11 +0700

    Create Job Model

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Job
 
 # Register your models here.
+
+
+class JobsAdmin(admin.ModelAdmin):
+    list_display = ('summary',)
+
+
+admin.site.register(Job, JobsAdmin)
